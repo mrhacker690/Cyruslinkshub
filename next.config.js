e587e630c038a66abbdeb80817f654104a7199f8
alphaknightrider <rk4451801@gmail.com>
2026-07-04 23:26:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // This allows the build to finish even if there are small code errors
+    // This keeps the TypeScript error away
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // This allows the build to finish even if there are linting warnings
-    ignoreDuringBuilds: true,
   },
 }
 
