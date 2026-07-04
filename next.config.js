c106faa260b21d2a8a78e3f88d2489a6ac41bd85
alphaknightrider <rk4451801@gmail.com>
2026-07-05 00:29:03 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    // This keeps the TypeScript error away
-    ignoreBuildErrors: true,
-  },
-}
-
-module.exports = nextConfig
