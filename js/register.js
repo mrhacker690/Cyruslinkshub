9ea7a07c780fe076210f997a48a6dc900bfe2abe
alphaknightrider <rk4451801@gmail.com>
2026-07-05 22:47:56 +0530

    Update register.js

--- js/register.js
+++ js/register.js
@@ -93,3 +93,16 @@ registerForm.addEventListener("submit", async (e) => {
     }
 
 });
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+
+onAuthStateChanged(auth,(user)=>{
+
+    if(user){
+
+        window.location.href="dashboard.html";
+
+    }
+
+});
